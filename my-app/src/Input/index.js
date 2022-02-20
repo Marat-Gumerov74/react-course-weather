16a8a457a8b37c8aef25bd898a7ba6771680f013
Marat Gumerov <gumerovm74@gmail.com>
2022-02-20 23:45:20 +0300

    step004 - listeners

--- my-app/src/Input/index.js
+++ my-app/src/Input/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import '../App.css';
 
 export const Input = () => {
+    const currentDate = new Date().toDateString();
+
     const handleOnChange = (e) => {
         console.log('e');
     }
+
+    const handleOnClick = (value) => () => {
+
+    }
+
     return (
         <div className="InputWrap">
             <input className="Input" onChange={handleOnChange}/>
-            <button className="Button">+</button>
+            <button className="Button" onClick={handleOnClick(currentDate)}>+</button>
         </div>
     );
 };
