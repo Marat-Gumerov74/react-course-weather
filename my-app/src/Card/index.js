679c7ff2f429d2b97217e4b0bd16c6d725aa4c6a
Marat Gumerov <gumerovm74@gmail.com>
2022-02-20 23:24:57 +0300

    step002 - add Card + css Card

---
+++ my-app/src/Card/index.js
@@ -0,0 +1,19 @@
+import React from 'react';    
+import '../App.css';
+
+export const Card = () => {
+    return (
+        <div className="Card">
+            <div className='MainInfo'>
+                <img className='Icon' alt="icon" src='http://openweathermap.org/img/wn/10d@2x.png' />
+                <div className='Title'>New Yord</div>
+                <div className='Decriprion'>Cloudy</div>
+                <div className='Temperature'>20</div>
+            </div>
+            <div className='Information'>
+                <div>Himidity:15</div>
+                <div>Feels like:19</div>
+            </div>
+        </div>
+    );
+};
