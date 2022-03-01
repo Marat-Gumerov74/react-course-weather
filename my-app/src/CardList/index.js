20635c79e746d8f525b1cc21adaa2f057b026908
Marat Gumerov <gumerovm74@gmail.com>
2022-03-01 13:28:00 +0300

    step016-add clkass Components

--- my-app/src/CardList/index.js
+++ my-app/src/CardList/index.js
@@ -1,16 +1,43 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../App';
+import React from 'react';
 import '../App.css';
 
 import { Card } from '../Card'
 
-export const CardList = () => {
-    const { state: {citiesList} } = useContext(GlobalContext);
-    return (
-        <div className="CardList">
-            {
-                citiesList.map(city => <Card key={city} city={city} />)
-            }
-        </div>
-    )
+export class CardList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orderBy: 'asc',
+        }
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleOnChange(e){
+        this.setState({
+            orderBy: e.target.value,
+        })
+    };
+
+    render() {
+        console.log(this.props);
+        const {orderBy} = this.state;
+        const {citiesList} = this.props;
+        const sortedCitiesList = citiesList.sort();
+        if (orderBy === 'desc') {
+            sortedCitiesList.reverse();
+        }
+        return (
+            <>
+                <select className='Select' value={orderBy} onChange={this.handleOnChange}>
+                    <option value="desc">By name desc</option>
+                    <option value="asc">By name asc</option>
+                </select>
+                <div className="CardList">
+                    {
+                        citiesList.map(city => <Card key={city} city={city} />)
+                    }
+                </div>
+            </>
+        )
+    }
 };
