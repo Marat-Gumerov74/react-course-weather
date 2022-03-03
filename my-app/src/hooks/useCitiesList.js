b865c171477f6bc14712584eb780843dfd5f1baa
Marat Gumerov <gumerovm74@gmail.com>
2022-03-03 21:18:20 +0300

    step017-refactoring code

--- my-app/src/hooks/useCitiesList.js
+++ my-app/src/hooks/useCitiesList.js
@@ -2,52 +2,52 @@ import { useEffect, useReducer } from 'react';
 
 const initialState = {
     inputValue: '',
-    editingCity:'',
+    editingCity: '',
     citiesList: JSON.parse(localStorage.getItem('citiesList')) || [],
-}
+};
 
-
-const reduser = (state, action) => {
+const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_CITY': {
-            const newState = {...state, citiesList: [...state.citiesList, action.payload]};
+            const newState = { ...state, citiesList: [...state.citiesList, action.payload] };
             return newState;
         }
         case 'DELETE_CITY': {
             const oldArray = state.citiesList;
-            const newArray = oldArray.filter( el => el !== action.payload);
-            return {...state, citiesList: newArray};
+            const newArray = oldArray.filter(el => el !== action.payload);
+            return { ...state, citiesList: newArray };
         }
         case 'EDIT_CITY': {
-            return {...state, inputValue: action.payload, editingCity: action.payload};
+            return { ...state, inputValue: action.payload, editingCity: action.payload };
         }
         case 'EDIT_CITY_DONE': {
             const { editingCity } = state;
             const oldArray = state.citiesList;
-            const filteredArray = oldArray.filter( el => el !== editingCity);
-            const newArray = [...filteredArray, action.payload]
+            const filteredArray = oldArray.filter(el => el !== editingCity);
+            const newArray = [...filteredArray, action.payload];
             return {
                 ...state,
                 citiesList: newArray,
                 inputValue: initialState.inputValue,
-                editingCity: initialState.editingCity
+                editingCity: initialState.editingCity,
             };
         }
         case 'CHANGE_INPUT_VALUE': {
-            return {...state, inputValue: action.payload};
+            return { ...state, inputValue: action.payload };
         }
         case 'RESET_INPUT_VALUE': {
-            return {...state, inputValue: initialState.inputValue};
+            return { ...state, inputValue: initialState.inputValue };
         }
-        default: return initialState;
+        default:
+            return initialState;
     }
 }
 
 export const useCitiesList = () => {
-    const [state, dispatch] = useReducer(reduser, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
     const { citiesList } = state;
     useEffect(() => {
         localStorage.setItem('citiesList', JSON.stringify(citiesList));
     }, [citiesList]);
-    return [citiesList, dispatch];
+    return [state, dispatch];
 }
